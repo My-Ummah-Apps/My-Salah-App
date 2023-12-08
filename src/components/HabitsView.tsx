9a4a6472cb8ce1c30af93c92577b175d434e5c64
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-12-08 09:13:59 +0000

    table now fully populated, all salah and dates are shown, each table cell detects what salah and date the user has clicked on

--- src/components/HabitsView.tsx
+++ src/components/HabitsView.tsx
@@ -1,37 +1,52 @@
 import { useState } from "react";
-
-useState;
+// import { v4 as uuidv4 } from "uuid";
 
 const salahObjects: any[] = [
   {
     salahName: "Fajr",
+    completedDates: ["01.12", "02.12", "", "04.12", ""],
+  },
+  {
+    salahName: "Zohar",
+    completedDates: ["01.12", "", "03.12", "04.12", "05.12"],
+  },
+  {
+    salahName: "Asar",
+    completedDates: ["01.12", "", "", "", "05.12"],
+  },
+  {
+    salahName: "Maghrib",
+    completedDates: ["01.12", "02.12", "03.12", "", ""],
+  },
+  {
+    salahName: "Isha",
     completedDates: ["01.12", "", "03.12", "", "05.12"],
   },
 ];
-salahObjects;
 
 const dates: any[] = ["01.12", "02.12", "03.12", "04.12", "05.12"];
 
-const completedDates: any[] = ["01.12", "", "03.12", "04.12", "05.12"];
-let newArray: number[] = [];
-for (let i = 0; i < dates.length; i++) {
-  if (dates[i] == completedDates[i]) {
-    console.log(i);
-    newArray.push(dates[i]);
-    console.log(newArray);
-  }
-}
-
-function grabDate(e: any) {
-  const cell = e.target as HTMLTableCellElement;
-  const columnIndex = cell.cellIndex;
-  const tableHeadDate =
-    e.target.parentElement.parentElement.parentElement.children[0].children[0]
-      .cells[columnIndex].textContent;
-  console.log(tableHeadDate);
-}
-
 const HabitsView = () => {
+  // const [salahStatus, setSalahStatus] = useState("");
+
+  function grabDate(e: any) {
+    const cell = e.target as HTMLTableCellElement;
+    const columnIndex = cell.cellIndex;
+    const selectedSalah: string = e.target.parentElement.cells[0].innerText;
+    const tableHeadDate =
+      e.target.parentElement.parentElement.parentElement.children[0].children[0]
+        .cells[columnIndex].textContent;
+    console.log(selectedSalah, tableHeadDate);
+    const salahName: any = salahObjects.find(
+      (salah) => salah.salahName === selectedSalah
+    );
+    if (salahName.completedDates.includes(tableHeadDate)) {
+      console.log("date exists");
+    } else {
+      console.log("date does not exist");
+    }
+  }
+
   return (
     <table
       onClick={(e) => {
@@ -50,40 +65,32 @@ const HabitsView = () => {
       <tbody>
         <tr>
           <td>Fajr</td>
-
-          {dates.map((date: any) => {
-            date;
-            return (
-              <td>
-                {salahObjects[0].completedDates.map((date: any) => {
-                  return date !== "" && date ? <td>T</td> : <td>X</td>;
-                })}
-              </td>
-            );
+          {salahObjects[0].completedDates.map((date: string) => {
+            return date !== "" && date ? <td>T</td> : <td>X</td>;
           })}
         </tr>
         <tr>
           <td>Zohar</td>
-          {completedDates.map((date) => {
-            return date !== "" ? <td>T</td> : <td>X</td>;
+          {salahObjects[1].completedDates.map((date: string) => {
+            return date !== "" && date ? <td>T</td> : <td>X</td>;
           })}
         </tr>
         <tr>
           <td>Asar</td>
-          {completedDates.map((date) => {
-            return date !== "" ? <td>T</td> : <td>X</td>;
+          {salahObjects[2].completedDates.map((date: string) => {
+            return date !== "" && date ? <td>T</td> : <td>X</td>;
           })}
         </tr>
         <tr>
           <td>Maghrib</td>
-          {completedDates.map((date) => {
-            return date !== "" ? <td>T</td> : <td>X</td>;
+          {salahObjects[3].completedDates.map((date: string) => {
+            return date !== "" && date ? <td>T</td> : <td>X</td>;
           })}
         </tr>
         <tr>
           <td>Isha</td>
-          {completedDates.map((date) => {
-            return date !== "" ? <td>T</td> : <td>X</td>;
+          {salahObjects[4].completedDates.map((date: string) => {
+            return date !== "" && date ? <td>T</td> : <td>X</td>;
           })}
         </tr>
       </tbody>
