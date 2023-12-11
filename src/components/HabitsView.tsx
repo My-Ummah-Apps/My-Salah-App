e236f7037cff28390aa7aaf0ce7ad526bf0beb79
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-12-11 18:08:14 +0000

    feat-modal now opens and closes, icons added to within modal, changed the structure of the dates array within the salah array which keeps a track of dates and relevant status of that date and prayer

--- src/components/HabitsView.tsx
+++ src/components/HabitsView.tsx
@@ -4,34 +4,64 @@ import ReactModal from "react-modal";
 
 // import { v4 as uuidv4 } from "uuid";
 
-const salahObjects: any[] = [
-  {
-    salahName: "Fajr",
-    completedDates: ["01.12", "02.12", "", "04.12", ""],
-  },
-  {
-    salahName: "Zohar",
-    completedDates: ["01.12", "", "03.12", "04.12", "05.12"],
-  },
-  {
-    salahName: "Asar",
-    completedDates: ["01.12", "", "", "", "05.12"],
-  },
-  {
-    salahName: "Maghrib",
-    completedDates: ["01.12", "02.12", "03.12", "", ""],
-  },
-  {
-    salahName: "Isha",
-    completedDates: ["01.12", "", "03.12", "", "05.12"],
-  },
-];
-
-const dates: any[] = ["01.12", "02.12", "03.12", "04.12", "05.12"];
+const dates: string[] = ["01.12", "02.12", "03.12", "04.12", "05.12"];
 
 const HabitsView = () => {
+  6;
+  const [salahObjects, setSalahObjects]: any[] = useState([
+    {
+      salahName: "Fajr",
+      completedDates: [
+        { "01.12": "Home" },
+        { "02.12": "Masjid" },
+        "",
+        { "04.12": "Masjid" },
+        "",
+      ],
+    },
+    {
+      salahName: "Zohar",
+      completedDates: [
+        "",
+        { "02.12": "Masjid" },
+        "",
+        { "04.12": "Masjid" },
+        "",
+      ],
+    },
+    {
+      salahName: "Asar",
+      completedDates: [
+        { "01.12": "Home" },
+        { "02.12": "Masjid" },
+        "",
+        { "04.12": "Masjid" },
+        "",
+      ],
+    },
+    {
+      salahName: "Maghrib",
+      completedDates: [
+        { "01.12": "Home" },
+        { "02.12": "Masjid" },
+        "",
+        { "04.12": "Masjid" },
+        "",
+      ],
+    },
+    {
+      salahName: "Isha",
+      completedDates: [
+        { "01.12": "Home" },
+        { "02.12": "Masjid" },
+        "",
+        { "04.12": "Masjid" },
+        "",
+      ],
+    },
+  ]);
   const [showModal, setShowModal] = useState(false);
-  //   const [showModal, setShowModal] = useState(false);
+  const [dateAndSalah, setDateAndSalah]: any = useState("");
 
   function grabDate(e: any) {
     const cell = e.target as HTMLTableCellElement;
@@ -46,6 +76,8 @@ const HabitsView = () => {
     );
     if (salahName.completedDates.includes(tableHeadDate)) {
       console.log("date exists");
+      setDateAndSalah({ date: tableHeadDate, salah: selectedSalah });
+      //   console.log(dateAndSalah);
     } else {
       console.log("date does not exist");
     }
@@ -53,8 +85,12 @@ const HabitsView = () => {
 
   return (
     <section>
-      <ModalOptions showModal={showModal} />
-      {/* <ReactModal ariaHideApp={false} isOpen={showModal} /> */}
+      <ModalOptions
+        setShowModal={setShowModal}
+        showModal={showModal}
+        setSalahObjects={setSalahObjects}
+        salahObjects={salahObjects}
+      />
       <table
         onClick={(e) => {
           grabDate(e);
@@ -73,32 +109,57 @@ const HabitsView = () => {
         <tbody>
           <tr>
             <td>Fajr</td>
-            {salahObjects[0].completedDates.map((date: string) => {
-              return date !== "" && date ? <td>T</td> : <td>X</td>;
+            {salahObjects[0].completedDates.map((date: any) => {
+              if (typeof date === "object" && date !== null) {
+                const [key, value]: any = Object.entries(date)[0];
+                return <td>{value}</td>;
+              } else {
+                return <td>X</td>;
+              }
             })}
           </tr>
           <tr>
             <td>Zohar</td>
-            {salahObjects[1].completedDates.map((date: string) => {
-              return date !== "" && date ? <td>T</td> : <td>X</td>;
+            {salahObjects[1].completedDates.map((date: any) => {
+              if (typeof date === "object" && date !== null) {
+                const [key, value]: any = Object.entries(date)[0];
+                return <td>{value}</td>;
+              } else {
+                return <td>X</td>;
+              }
             })}
           </tr>
           <tr>
             <td>Asar</td>
-            {salahObjects[2].completedDates.map((date: string) => {
-              return date !== "" && date ? <td>T</td> : <td>X</td>;
+            {salahObjects[2].completedDates.map((date: any) => {
+              if (typeof date === "object" && date !== null) {
+                const [key, value]: any = Object.entries(date)[0];
+                return <td>{value}</td>;
+              } else {
+                return <td>X</td>;
+              }
             })}
           </tr>
           <tr>
             <td>Maghrib</td>
-            {salahObjects[3].completedDates.map((date: string) => {
-              return date !== "" && date ? <td>T</td> : <td>X</td>;
+            {salahObjects[3].completedDates.map((date: any) => {
+              if (typeof date === "object" && date !== null) {
+                const [key, value]: any = Object.entries(date)[0];
+                return <td>{value}</td>;
+              } else {
+                return <td>X</td>;
+              }
             })}
           </tr>
           <tr>
             <td>Isha</td>
-            {salahObjects[4].completedDates.map((date: string) => {
-              return date !== "" && date ? <td>T</td> : <td>X</td>;
+            {salahObjects[4].completedDates.map((date: any) => {
+              if (typeof date === "object" && date !== null) {
+                const [key, value]: any = Object.entries(date)[0];
+                return <td>{value}</td>;
+              } else {
+                return <td>X</td>;
+              }
             })}
           </tr>
         </tbody>
