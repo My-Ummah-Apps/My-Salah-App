3d8f6ec8222251bce3b4ec594952d76d0fef3cc2
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-12-26 13:35:50 +0000

    added gitignore file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 
@@ -7,13 +7,78 @@ import NavBar from "./components/NavBar";
 import SettingsPage from "./pages/SettingsPage";
 import StatsPage from "./pages/StatsPage";
 
+type salahObjectsType = {
+  salahName: string;
+  completedDates: { [date: string]: string }[];
+};
+
 function App() {
+  const [salahObjects, setSalahObjects] = useState<salahObjectsType[]>([
+    // const [salahObjects, setSalahObjects]: any[] = useState([
+    {
+      salahName: "Fajr",
+      completedDates: [
+        { "23.12.23": "Home" },
+        { "22.12.23": "Masjid" },
+        { "20.12.23": "Masjid" },
+        { "19.12.23": "Home" },
+      ],
+    },
+    {
+      salahName: "Zohar",
+      completedDates: [
+        { "24.12.23": "Masjid" },
+        { "22.12.23": "Home" },
+        { "20.12.23": "Masjid" },
+      ],
+    },
+    {
+      salahName: "Asar",
+      completedDates: [
+        { "23.12.23": "Home" },
+        { "22.12.23": "Masjid" },
+        { "21.12.23": "Home" },
+      ],
+    },
+    {
+      salahName: "Maghrib",
+      completedDates: [
+        { "24.12.23": "Masjid" },
+        { "21.12.23": "Masjid" },
+        { "18.12.23": "Home" },
+      ],
+    },
+    {
+      salahName: "Isha",
+      completedDates: [{ "23.12.23": "Home" }, { "21.12.23": "Masjid" }],
+    },
+  ]);
+
+  // localStorage.setItem("storedSalahTrackingData", JSON.stringify(salahObjects));
+
+  // useEffect(() => {
+  //   const storedSalahTrackingData = localStorage.getItem(
+  //     "storedSalahTrackingData"
+  //   );
+  //   storedSalahTrackingData
+  //     ? setSalahObjects(JSON.parse(storedSalahTrackingData))
+  //     : setSalahObjects([]);
+  // }, []);
+
   return (
     <BrowserRouter>
       <section className="App">
         <Routes>
           <Route path="StatsPage" element={<StatsPage />} />
-          <Route index element={<MainPage />} />
+          <Route
+            index
+            element={
+              <MainPage
+                setSalahObjects={setSalahObjects}
+                salahObjects={salahObjects}
+              />
+            }
+          />
           <Route path="SettingsPage" element={<SettingsPage />} />
         </Routes>
         <NavBar />
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,16 @@
 import HabitsView from "../components/HabitsView";
 
-const MainPage = () => {
+const MainPage = ({
+  setSalahObjects: setSalahObjects,
+  salahObjects: salahObjects,
+}) => {
   return (
-    <div className="">
-      <HabitsView />
+    <div className="overflow-x-auto w-5/5 main-page-wrap">
+      <h1 className="p-5 text-xl text-white">Prayer Tracker</h1>
+      <HabitsView
+        setSalahObjects={setSalahObjects}
+        salahObjects={salahObjects}
+      />
     </div>
   );
 };
