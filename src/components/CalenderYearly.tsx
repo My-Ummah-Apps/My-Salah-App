3429f4b5ea63d6b7c8b91d60a9f52a44b3427da1
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2024-01-17 10:41:18 +0000

    wip - adding yearly calender

--- src/components/CalenderYearly.tsx
+++ src/components/CalenderYearly.tsx
@@ -16,14 +16,24 @@ import {
   startOfWeek,
   startOfISOWeek,
   startOfMonth,
+  eachMonthOfInterval,
+  endOfYear,
 } from "date-fns";
 
 const CalenderYearly = ({ salahObjects }) => {
   //   const today = startOfToday(); // Todays date, example: Wed Jan 10 2024 00:00:00 GMT+0000 (Greenwich Mean Time) (object)
   const today = startOfYear(new Date());
+  const formattedYear = format(today, "yyyy");
+  //   console.log(today);
+
+  const [currentYear, setCurrentYear] = useState();
+
+  //   console.log(format(getMonthsForYear(2023), "MMM-yyyy"));
+
+  //   setcurrentMonth(format(firstDayOfNextMonth, "MMM-yyyy"))
 
   const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-  //   const days = [""];
+
   const colStartClasses = [
     // "",
     // "col-start-2",
@@ -33,7 +43,6 @@ const CalenderYearly = ({ salahObjects }) => {
     // "col-start-6",
     // "col-start-7",
   ];
-  let dateColor: string;
 
   let datesArray = [];
   const allDatesArray = salahObjects.reduce((value, salah) => {
@@ -42,8 +51,6 @@ const CalenderYearly = ({ salahObjects }) => {
     });
     return datesArray;
   }, []);
-
-  // console.log(allDatesArray);
 
   let dArray = [];
 
@@ -63,7 +70,9 @@ const CalenderYearly = ({ salahObjects }) => {
       return dArray;
     });
 
+    let dateColor: string;
     let color;
+
     if (increment === 0) {
       // console.log("increment > 2");
       color = "red";
@@ -101,14 +110,51 @@ const CalenderYearly = ({ salahObjects }) => {
     );
   };
 
-  let firstDayOfMonth = parse(currentMonth, "MMM-yyyy", new Date()); // Returns Mon Jan 01 2024 00:00:00 GMT+0000 (Greenwich Mean Time) (object)
+  const getAllDatesInMonth = (monthString) => {
+    // const monthDate = parse(monthString, "MMM-yyyy", new Date());
+    const monthDate = new Date(monthString);
+    // console.log(monthString);
+
+    const startOfMonthDate = startOfMonth(monthDate);
+    const endOfMonthDate = endOfMonth(monthDate);
+
+    // Get all dates of the month
+    const datesOfMonth = eachDayOfInterval({
+      start: startOfMonthDate,
+      end: endOfMonthDate,
+    });
+    // console.log(datesOfMonth);
+  };
+  //   const monthString = "Mar-2024";
+
+  const getMonthsForYear = (year) => {
+    // Get all months of the year
+    const months = eachMonthOfInterval({
+      start: startOfMonth(new Date(year, 0, 1)),
+      end: endOfYear(new Date(year, 11, 31)),
+    });
+    const dates = eachDayOfInterval({
+      start: startOfMonth(new Date(2023, 0, 1)),
+      end: endOfYear(new Date(2023, 11, 31)),
+    });
+    console.log(months[6]);
+    getAllDatesInMonth(months[6]);
+    return { months, dates };
+  };
+
+  const { months, dates } = getMonthsForYear(2023);
+  console.log(dates);
+
+  let firstDayOfMonth = parse(currentMonth, "MMM-yyyy", new Date());
+
+  //   console.log(dates);
 
   const daysInMonth = eachDayOfInterval({
     // The eachDayOfInterval function gives dates between (and including) the two dates that are passed in.
     start: startOfWeek(firstDayOfMonth, { weekStartsOn: 1 }), // Gives first day of month
-
     end: endOfWeek(endOfMonth(firstDayOfMonth)), // Once we have the first day of the month, endOfMonth calculates the last day of the month, then, endOfWeek is used to find the end of the week for that particular date
   }); // The result here is an array of objects, object at 0 position is Sun Dec 31 2023 00:00:00 GMT+0000 (Greenwich Mean Time), array ends at index 34, which is Sat Feb 03 2024 00:00:00 GMT+0000 (Greenwich Mean Time)
+  console.log(daysInMonth);
 
   const getPrevMonth = (event: React.MouseEvent<SVGSVGElement>) => {
     event.preventDefault();
@@ -122,65 +168,32 @@ const CalenderYearly = ({ salahObjects }) => {
     setcurrentMonth(format(firstDayOfNextMonth, "MMM-yyyy"));
   };
 
+  const CalenderYearly = ({ salahObjects }) => {
+    const year = 2024; // Replace with the desired year
+    const months = [];
+  
+    // Generate an array of 12 months
+    for (let i = 0; i < 12; i++) {
+      const currentMonth = addMonths(new Date(year, 0, 1), i);
+      months.push(currentMonth);
+    }
+
   return (
     <>
       <h1 className="pt-5 text-3xl">Yearly</h1>
-      <div className="flex items-center justify-center w-screen h-screen p-8">
-        <div className="w-[900px] h-[600px]">
-          <div className="flex items-center justify-between">
-            <p className="text-xl font-semibold">
-              {format(firstDayOfMonth, "MMMM yyyy")}
-            </p>
-            <div className="flex items-center gap-6 justify-evenly sm:gap-12">
-              <IoChevronBackSharp
-                className="w-6 h-6 cursor-pointer"
-                onClick={getPrevMonth}
-              />
-              <IoChevronForward
-                className="w-6 h-6 cursor-pointer"
-                onClick={getNextMonth}
-              />
-            </div>
-          </div>
-          <hr className="my-6" />
-          <div className="grid grid-cols-7 gap-6 sm:gap-12 place-items-center">
-            {days.map((day, index) => {
-              return (
-                <div key={index} className="font-semibold">
-                  {/* {capitalizeFirstLetter(day)} */}
-                  {day}
-                </div>
-              );
-            })}
-          </div>
-          <div className="grid grid-cols-7 gap-6 mt-8 sm:gap-12 place-items-center">
-            {daysInMonth.map((day, index) => {
-              return (
-                <div key={index} className="">
-                  <p
-                    style={{
-                      backgroundColor: howManyDatesExist(
-                        format(day, "dd.MM.yy")
-                      ),
-                    }}
-                    className={`cursor-pointer flex items-center justify-center font-semibold h-8 w-8 rounded-full  hover:text-white ${
-                      // isSameMonth(day, today) ? "text-gray-900" : "text-gray-400"
+      <div className="grid grid-cols-2 gap-4">
+      {months.map((month, index) => {
+        const monthString = format(month, 'MMM-yyyy');
+        const datesOfMonth = getDatesOfMonth(monthString);
 
-                      isDayInSpecificMonth(day, currentMonth)
-                        ? "text-gray-900"
-                        : "text-gray-400"
-                    } 
-
-                    `}
-                  >
-                    {format(day, "d")}
-                  </p>
-                </div>
-              );
-            })}
+        return (
+          <div key={index}>
+            <h2 className="text-lg font-bold">{monthString}</h2>
+            <CalenderMonthly dates={datesOfMonth} salahObjects={salahObjects} />
           </div>
-        </div>
-      </div>
+        );
+      })}
+    </div>
     </>
   );
 };
