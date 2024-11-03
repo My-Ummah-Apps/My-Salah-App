b220e013109708d43edaab97aa8106f8e1ae0a43
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2024-11-03 10:44:18 +0000

    fix: resolved issue calendar dates not aligning with the correct days

--- src/components/Stats/Calendar.tsx
+++ src/components/Stats/Calendar.tsx
@@ -180,87 +180,88 @@ const Calendar = ({
           })}
         </div>
         <div className="grid grid-cols-7 px-2 gap-y-5 place-items-center month-dates-wrap">
-          {monthlyDates(formattedMonths[index]).map(
-            (date) =>
-              isDayInSpecificMonth(date, formattedMonths[index]) && (
-                <div
-                  onClick={() => {
-                    // if (date <= todaysDate) {
-                    if (date >= userStartDateParsed && date <= todaysDate) {
-                      const formattedDate = format(date, "yyyy-MM-dd");
-                      setClickedDate(formattedDate);
-                      setShowDailySalahDataModal(true);
-                    }
-                  }}
-                  key={format(date, "yyyy-MM-dd")}
-                  className="relative flex items-center justify-center individual-date"
+          {monthlyDates(formattedMonths[index]).map((date) =>
+            isDayInSpecificMonth(date, formattedMonths[index]) ? (
+              <div
+                onClick={() => {
+                  // if (date <= todaysDate) {
+                  if (date >= userStartDateParsed && date <= todaysDate) {
+                    const formattedDate = format(date, "yyyy-MM-dd");
+                    setClickedDate(formattedDate);
+                    setShowDailySalahDataModal(true);
+                  }
+                }}
+                key={format(date, "yyyy-MM-dd")}
+                className="relative flex items-center justify-center individual-date"
+              >
+                {determineRadialColors(date)}
+                <svg
+                  className="absolute"
+                  xmlns="http://www.w3.org/2000/svg"
+                  id="svg"
+                  viewBox="0 0 150 150"
+                  style={{ height: "35px", width: "35px" }}
                 >
-                  {determineRadialColors(date)}
-                  <svg
-                    className="absolute"
-                    xmlns="http://www.w3.org/2000/svg"
-                    id="svg"
-                    viewBox="0 0 150 150"
-                    style={{ height: "35px", width: "35px" }}
-                  >
-                    <desc>Created with Snap</desc>
-                    <defs />
-                    <path
-                      d="M 86.1150408904588 8.928403485284022 A 67 67 0 0 1 134.40308587902058 44.011721763710284"
-                      style={{
-                        strokeWidth: "11px",
-                        strokeLinecap: "round",
-                      }}
-                      fill="none"
-                      stroke={zoharColor}
-                    />
-                    <path
-                      d="M 141.272558935669 65.15378589922615 A 67 67 0 0 1 122.82816700032245 121.91978731185029"
-                      style={{
-                        strokeWidth: "11px",
-                        strokeLinecap: "round",
-                      }}
-                      fill="none"
-                      stroke={asarColor}
-                    />
-                    <path
-                      d="M 104.84365305321519 134.98630153992926 A 67 67 0 0 1 45.15634694678482 134.98630153992926"
-                      style={{
-                        strokeWidth: "11px",
-                        strokeLinecap: "round",
-                      }}
-                      fill="none"
-                      stroke={maghribColor}
-                    />
-                    <path
-                      d="M 27.171832999677548 121.91978731185029 A 67 67 0 0 1 8.72744106433099 65.15378589922618"
-                      style={{
-                        strokeWidth: "11px",
-                        strokeLinecap: "round",
-                      }}
-                      fill="none"
-                      stroke={ishaColor}
-                    />
-                    <path
-                      d="M 15.596914120979442 44.01172176371027 A 67 67 0 0 1 63.884959109541164 8.928403485284022"
-                      style={{
-                        strokeWidth: "11px",
-                        strokeLinecap: "round",
-                      }}
-                      fill="none"
-                      stroke={fajrColor}
-                    />
-                  </svg>
-                  <p
-                    //
-                    className={` text-sm cursor-pointer flex items-center justify-center font-semibold h-6 w-6 hover:text-white
+                  <desc>Created with Snap</desc>
+                  <defs />
+                  <path
+                    d="M 86.1150408904588 8.928403485284022 A 67 67 0 0 1 134.40308587902058 44.011721763710284"
+                    style={{
+                      strokeWidth: "11px",
+                      strokeLinecap: "round",
+                    }}
+                    fill="none"
+                    stroke={zoharColor}
+                  />
+                  <path
+                    d="M 141.272558935669 65.15378589922615 A 67 67 0 0 1 122.82816700032245 121.91978731185029"
+                    style={{
+                      strokeWidth: "11px",
+                      strokeLinecap: "round",
+                    }}
+                    fill="none"
+                    stroke={asarColor}
+                  />
+                  <path
+                    d="M 104.84365305321519 134.98630153992926 A 67 67 0 0 1 45.15634694678482 134.98630153992926"
+                    style={{
+                      strokeWidth: "11px",
+                      strokeLinecap: "round",
+                    }}
+                    fill="none"
+                    stroke={maghribColor}
+                  />
+                  <path
+                    d="M 27.171832999677548 121.91978731185029 A 67 67 0 0 1 8.72744106433099 65.15378589922618"
+                    style={{
+                      strokeWidth: "11px",
+                      strokeLinecap: "round",
+                    }}
+                    fill="none"
+                    stroke={ishaColor}
+                  />
+                  <path
+                    d="M 15.596914120979442 44.01172176371027 A 67 67 0 0 1 63.884959109541164 8.928403485284022"
+                    style={{
+                      strokeWidth: "11px",
+                      strokeLinecap: "round",
+                    }}
+                    fill="none"
+                    stroke={fajrColor}
+                  />
+                </svg>
+                <p
+                  //
+                  className={` text-sm cursor-pointer flex items-center justify-center font-semibold h-6 w-6 hover:text-white
               `}
-                  >
-                    {format(date, "d")}
-                    {/* {format(day, "dd")} */}
-                  </p>
-                </div>
-              )
+                >
+                  {format(date, "d")}
+                  {/* {format(day, "dd")} */}
+                </p>
+              </div>
+            ) : (
+              <div></div>
+            )
           )}
         </div>
       </div>
