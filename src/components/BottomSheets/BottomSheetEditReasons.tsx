121bd25e959ccfb61b031c7cf74e0a0ff80f744d
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-03-16 10:15:56 +0000

    style: add animation to max character text

--- src/components/BottomSheets/BottomSheetEditReasons.tsx
+++ src/components/BottomSheets/BottomSheetEditReasons.tsx
@@ -12,7 +12,7 @@ import {
 import Sheet from "react-modal-sheet";
 import { TiDelete } from "react-icons/ti";
 import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
 import { VscDebugRestart } from "react-icons/vsc";
 
 interface BottomSheetStartDateProps {
@@ -49,84 +49,87 @@ const BottomSheetEditReasons = ({
           <Sheet.Content>
             <Sheet.Scroller>
               <section className="mx-4 my-4">
-                <section>
-                  <section className="flex justify-between w-full">
-                    {" "}
-                    <section className="flex">
-                      <input
-                        className="p-1 rounded-md bg-zinc-800"
-                        onChange={(e) => {
-                          if (e.target.value.length > CHAR_LIMIT) return;
-                          setNewReasonInput(e.target.value);
-                          setCharCount(CHAR_LIMIT - e.target.value.length);
-                        }}
-                        type="text"
-                        dir="auto"
-                        maxLength={CHAR_LIMIT}
-                        value={newReasonInput}
-                      ></input>
-                      <button
-                        className="px-2 ml-2 bg-blue-600 rounded-md"
-                        onClick={async () => {
-                          if (
-                            userPreferences.reasons.some(
-                              (item) =>
-                                item.toLocaleLowerCase() ===
-                                newReasonInput.toLocaleLowerCase()
-                            )
-                          ) {
-                            showAlert(
-                              "Duplicate Reason",
-                              "This reason already exists"
-                            );
-                            return;
-                          }
-                          const updatedReasons = [
-                            ...userPreferences.reasons,
-                            newReasonInput,
-                          ];
-                          await modifyDataInUserPreferencesTable(
-                            "reasons",
-                            updatedReasons
-                          );
-                          setNewReasonInput("");
-                          setCharCount(CHAR_LIMIT);
-                          showToast(`${newReasonInput} added`, "short");
-                        }}
-                      >
-                        Add
-                      </button>
-                    </section>
+                <section className="flex justify-between w-full">
+                  {" "}
+                  <section className="flex">
+                    <input
+                      className="p-1 rounded-md bg-zinc-800"
+                      onChange={(e) => {
+                        if (e.target.value.length > CHAR_LIMIT) return;
+                        setNewReasonInput(e.target.value);
+                        setCharCount(CHAR_LIMIT - e.target.value.length);
+                      }}
+                      type="text"
+                      dir="auto"
+                      maxLength={CHAR_LIMIT}
+                      value={newReasonInput}
+                    ></input>
                     <button
+                      className="px-2 ml-2 bg-blue-600 rounded-md"
                       onClick={async () => {
-                        const reasonConfirmMsgRes = await showConfirmMsg(
-                          "Reset Reasons?",
-                          "This will reset all reasons to the app’s default reasons. Are you sure you want to proceed?"
-                        );
-                        if (!reasonConfirmMsgRes) return;
+                        if (
+                          userPreferences.reasons.some(
+                            (item) =>
+                              item.toLocaleLowerCase() ===
+                              newReasonInput.toLocaleLowerCase()
+                          )
+                        ) {
+                          showAlert(
+                            "Duplicate Reason",
+                            "This reason already exists"
+                          );
+                          return;
+                        }
+                        const updatedReasons = [
+                          ...userPreferences.reasons,
+                          newReasonInput,
+                        ];
                         await modifyDataInUserPreferencesTable(
                           "reasons",
-                          defaultReasons.split(",")
+                          updatedReasons
                         );
-                        showToast("Default Reasons Restored", "short");
+                        setNewReasonInput("");
+                        setCharCount(CHAR_LIMIT);
+                        showToast(`${newReasonInput} added`, "short");
                       }}
                     >
-                      <VscDebugRestart className="text-2xl" />
+                      Add
                     </button>
                   </section>
+                  <button
+                    onClick={async () => {
+                      const reasonConfirmMsgRes = await showConfirmMsg(
+                        "Reset Reasons?",
+                        "This will reset all reasons to the app’s default reasons. Are you sure you want to proceed?"
+                      );
+                      if (!reasonConfirmMsgRes) return;
+                      await modifyDataInUserPreferencesTable(
+                        "reasons",
+                        defaultReasons.split(",")
+                      );
+                      showToast("Default Reasons Restored", "short");
+                    }}
+                  >
+                    <VscDebugRestart className="text-2xl" />
+                  </button>
+                </section>
 
+                {newReasonInput.length > 0 && (
                   <motion.p
+                    layout
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.3 }}
                     style={{
-                      visibility:
-                        newReasonInput.length > 0 ? "visible" : "hidden",
                       color: charCount === 0 ? "red" : "white",
                     }}
                     className="mt-1 text-xs"
                   >
                     {`${charCount} characters left`}
                   </motion.p>
-                </section>
-                <ul>
+                )}
+
+                <ul className="mt-3">
                   <AnimatePresence>
                     {userPreferences.reasons
                       .sort((a, b) => a.localeCompare(b))
@@ -164,6 +167,7 @@ const BottomSheetEditReasons = ({
                   </AnimatePresence>
                 </ul>
               </section>
+
               <section className="flex justify-between w-full px-4 py-4"></section>
             </Sheet.Scroller>
           </Sheet.Content>
