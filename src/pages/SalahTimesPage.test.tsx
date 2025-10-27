3caad3e7cf4925dcda346875a5575b4c9223ecb5
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-10-27 11:11:02 +0000

    test: add test for opening onboarding modal on setup button click

--- src/pages/SalahTimesPage.test.tsx
+++ src/pages/SalahTimesPage.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import SalahTimesPage from "./SalahTimesPage";
 import { dictPreferencesDefaultValues } from "../utils/constants";
+import userEvent from "@testing-library/user-event";
 
 const mockUserPrefs = {
   ...dictPreferencesDefaultValues,
@@ -16,8 +17,19 @@ describe("Prayer Times", () => {
     const fallbackText = screen.getByText(/Salah Times Not Set/i);
     expect(fallbackText).toBeInTheDocument();
 
-    const autoDetectBtn = screen.getByText(/Set up Salah Times/i);
-    expect(autoDetectBtn).toBeInTheDocument();
+    const setUpBtn = screen.getByText(/Set up Salah Times/i);
+    expect(setUpBtn).toBeInTheDocument();
+  });
+
+  it("opens onboarding modal when setup button is clicked", () => {
+    const setUpBtn = screen.getByText(/Set Up Salah Times/i);
+    userEvent.click(setUpBtn);
+    const selectLocationText = screen.getByText(/Select Location/i);
+    expect(selectLocationText).toBeInTheDocument();
+    const autoDetectLocationBtn = screen.getByText(/Auto-Detect/i);
+    expect(autoDetectLocationBtn).toBeInTheDocument();
+    const manualLocationBtn = screen.getByText(/Add Manually/i);
+    expect(manualLocationBtn).toBeInTheDocument();
   });
 
   //   it("calculates times for location correctly", () => {});
