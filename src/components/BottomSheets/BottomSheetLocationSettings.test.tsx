03aff80b0df6e814e289c0711e073e2b177f0327
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-11-05 19:17:03 +0000

    test: add unit test for GPS button triggering native settings when permission is denied

--- src/components/BottomSheets/BottomSheetLocationSettings.test.tsx
+++ src/components/BottomSheets/BottomSheetLocationSettings.test.tsx
@@ -1,7 +1,7 @@
 // const autoDetectBtn = await screen.findByText(/gps/i);
 // expect(autoDetectBtn).toBeInTheDocument();
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BottomSheetLocationSettings from "./BottomSheetLocationSettings";
 import { vi } from "vitest";
 import userEvent from "@testing-library/user-event";
@@ -33,23 +33,51 @@ vi.mock("@ionic/react", async () => {
   };
 });
 
+vi.mock("capacitor-native-settings", () => ({
+  NativeSettings: {
+    open: vi.fn(),
+  },
+}));
+
+import { NativeSettings } from "capacitor-native-settings";
+
 vi.mock("@capacitor/geolocation", () => ({
   Geolocation: {
     getCurrentPosition: vi.fn().mockResolvedValue({
       coords: { latitude: 53.48, longitude: -3.44 },
     }),
-    checkPermissions: vi.fn().mockResolvedValue({ location: "granted" }),
-    requestPermissions: vi.fn().mockResolvedValue({ location: "granted" }),
+    checkPermissions: vi.fn().mockResolvedValue({
+      location: "granted",
+      coarseLocation: "granted",
+    }),
+    requestPermissions: vi.fn().mockResolvedValue({
+      location: "granted",
+      coarseLocation: "granted",
+    }),
   },
 }));
 
+import { Geolocation } from "@capacitor/geolocation";
+
 describe("Unit tests for GPS location button functionality", () => {
   let findMyLocationBtn: HTMLButtonElement;
-  beforeEach(async () => {
+  beforeEach(() => {
     render(<BottomSheetLocationSettings triggerId={"testId"} />);
     findMyLocationBtn = screen.getByText(/find my location/i);
-    await userEvent.click(findMyLocationBtn);
   });
 
-  it("");
+  it("triggers native settings alert when user taps on find my location button but permission has been denied", async () => {
+    vi.mocked(Geolocation.checkPermissions).mockResolvedValueOnce({
+      location: "denied",
+      coarseLocation: "denied",
+    });
+    await userEvent.click(findMyLocationBtn);
+
+    await waitFor(() => {
+      expect(NativeSettings.open).toHaveBeenCalled();
+    });
+    expect(
+      screen.findByText(/please turn on location permissions/i)
+    ).toBeInTheDocument();
+  });
 });
