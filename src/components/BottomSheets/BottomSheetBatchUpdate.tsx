46193b6ab662d36d13dcccabab4aa893b261257b
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2026-02-19 12:13:07 +0000

    feat: batch update modal UI complete, dates, status, salah selection, reasons options and notes input all added

--- src/components/BottomSheets/BottomSheetBatchUpdate.tsx
+++ src/components/BottomSheets/BottomSheetBatchUpdate.tsx
@@ -1,16 +1,22 @@
-import { IonCheckbox, IonModal } from "@ionic/react";
 import {
-  INITIAL_MODAL_BREAKPOINT,
-  MODAL_BREAKPOINTS,
-} from "../../utils/constants";
+  IonCheckbox,
+  IonContent,
+  IonInput,
+  IonModal,
+  IonTextarea,
+} from "@ionic/react";
 import { SQLiteDBConnection } from "@capacitor-community/sqlite";
 import {
   SalahNamesType,
   SalahStatusType,
   userPreferencesType,
 } from "../../types/types";
-import { useState } from "react";
-import { createLocalisedDate } from "../../utils/helpers";
+import { useEffect, useState } from "react";
+import {
+  INITIAL_MODAL_BREAKPOINT,
+  MODAL_BREAKPOINTS,
+  salahNamesArr,
+} from "../../utils/constants";
 
 interface BottomSheetBatchUpdateProps {
   dbConnection: React.MutableRefObject<SQLiteDBConnection | undefined>;
@@ -28,102 +34,205 @@ const BottomSheetBatchUpdate = ({
   fetchDataFromDB,
 }: BottomSheetBatchUpdateProps) => {
   type batchUpdateObj = {
-    dates: string[];
+    fromDate: string;
+    toDate: string;
     salahs: SalahNamesType[];
-    status: SalahStatusType[];
-    reasons: string;
+    status: SalahStatusType;
+    reasons: string[];
     notes: string;
   };
 
   const [batchUpdateObj, setBatchUpdateObj] = useState<batchUpdateObj>({
-    dates: [],
+    fromDate: "",
+    toDate: "",
     salahs: [],
-    status: [],
-    reasons: "",
+    status: "",
+    reasons: [],
     notes: "",
   });
+
+  useEffect(() => {
+    console.log("batchUpdateObj: ", batchUpdateObj);
+  }, [batchUpdateObj]);
+
+  const statusArr: SalahStatusType[] =
+    userPreferences.userGender === "male"
+      ? ["group", "male-alone", "late", "missed"]
+      : ["female-alone", "excused", "late", "missed"];
 
   return (
     <IonModal
       //   ref={modal}
-      style={{
-        "--width": "fit-content",
-        "--min-width": "250px",
-        "--height": "fit-content",
-        "--border-radius": "6px",
-        "--box-shadow": "0 28px 48px rgba(0, 0, 0, 0.4)",
-      }}
+      // style={{
+      //   "--width": "fit-content",
+      //   "--min-width": "250px",
+      //   "--height": "fit-content",
+      //   "--border-radius": "6px",
+      //   "--box-shadow": "0 28px 48px rgba(0, 0, 0, 0.4)",
+      // }}
       mode="ios"
       expandToScroll={false}
       // className="modal-fit-content"
       trigger={triggerId}
+      initialBreakpoint={INITIAL_MODAL_BREAKPOINT}
+      breakpoints={MODAL_BREAKPOINTS}
     >
-      <section className="p-10 pb-5">
-        <div className="text-center">
-          <p className="">From</p>
-          <input
-            className="text-[var(--ion-text-color)] bg-[var(--textarea-bg-color)] rounded-[0.3rem] border-none [color-scheme:dark] p-[0.3rem]"
-            placeholder="&#x1F5D3;"
-            onKeyDown={(e) => {
-              e.preventDefault();
+      <IonContent>
+        <section className="p-10 pb-5">
+          <div className="text-center">
+            <p className="">From</p>
+            <IonInput
+              className="text-[var(--ion-text-color)] bg-[var(--textarea-bg-color)] rounded-[0.3rem] border-none [color-scheme:dark] p-[0.3rem]"
+              placeholder="&#x1F5D3;"
+              onKeyDown={(e) => {
+                // e.preventDefault();
+              }}
+              onIonChange={(e) => {
+                setBatchUpdateObj((prev) => ({
+                  ...prev,
+                  fromDate: e.detail.value ?? "",
+                }));
+              }}
+              type="date"
+              dir="auto"
+              name="start-date-picker"
+              min="1950-01-01"
+              max={new Date().toISOString().split("T")[0]}
+            ></IonInput>
+          </div>
+          <div className="text-center">
+            <p className="mt-5">To</p>
+            <IonInput
+              className="text-[var(--ion-text-color)] bg-[var(--textarea-bg-color)] rounded-[0.3rem] border-none [color-scheme:dark] p-[0.3rem]"
+              placeholder="&#x1F5D3;"
+              onKeyDown={(e) => {
+                // e.preventDefault();
+              }}
+              onIonChange={(e) => {
+                setBatchUpdateObj((prev) => ({
+                  ...prev,
+                  toDate: e.detail.value ?? "",
+                }));
+              }}
+              // ref={datePickerRef}
+              type="date"
+              dir="auto"
+              name="start-date-picker"
+              min="1950-01-01"
+              max={new Date().toISOString().split("T")[0]}
+            ></IonInput>
+          </div>
+        </section>
+        <section>
+          {statusArr.map((status) => (
+            <IonCheckbox
+              style={{
+                "--size": "16px",
+                "--border-color": "#888888",
+              }}
+              key={status}
+              checked={batchUpdateObj.status === status}
+              onIonChange={() => {
+                setBatchUpdateObj((prev) => ({
+                  ...prev,
+                  status: status,
+                }));
+              }}
+              labelPlacement="stacked"
+            >
+              {status}
+            </IonCheckbox>
+          ))}
+        </section>
+        <section className="mb-5">
+          {salahNamesArr.map((salahName) => (
+            <IonCheckbox
+              style={{
+                "--size": "16px",
+                "--border-color": "#888888",
+              }}
+              key={salahName}
+              checked={batchUpdateObj.salahs.includes(salahName)}
+              onIonChange={() => {
+                setBatchUpdateObj((prev) => ({
+                  ...prev,
+                  salahs: prev.salahs.includes(salahName)
+                    ? prev.salahs.filter((s) => s !== salahName)
+                    : [...prev.salahs, salahName],
+                }));
+              }}
+              labelPlacement="stacked"
+            >
+              {salahName}
+            </IonCheckbox>
+          ))}
+        </section>
+        <section>
+          <p>Reasons</p>
+          {userPreferences.reasons.map((reason) => (
+            <IonCheckbox
+              style={{
+                "--size": "16px",
+                "--border-color": "#888888",
+              }}
+              checked={batchUpdateObj.reasons.includes(reason)}
+              key={reason}
+              labelPlacement="stacked"
+              onIonChange={() => {
+                setBatchUpdateObj((prev) => ({
+                  ...prev,
+                  reasons: prev.reasons.includes(reason)
+                    ? prev.reasons.filter((r) => r !== reason)
+                    : [...prev.reasons, reason],
+                }));
+              }}
+            >
+              {reason}
+            </IonCheckbox>
+          ))}
+        </section>
+        <div className="text-sm notes-wrap">
+          <IonTextarea
+            aria-label="notes"
+            autoGrow={true}
+            rows={1}
+            className="pl-2 rounded-lg text-[var(--ion-text-color)] bg-[var(--textarea-bg-color)]"
+            placeholder="Notes"
+            value={batchUpdateObj.notes}
+            onIonInput={(e) => {
+              setBatchUpdateObj((prev) => ({
+                ...prev,
+                notes: e.detail.value ?? "",
+              }));
             }}
-            // ref={datePickerRef}
-            type="date"
-            dir="auto"
-            name="start-date-picker"
-            min="1950-01-01"
-            max={new Date().toISOString().split("T")[0]}
-          ></input>
+          ></IonTextarea>
         </div>
-        <div className="text-center">
-          <p className="mt-5">To</p>
-          <input
-            className="text-[var(--ion-text-color)] bg-[var(--textarea-bg-color)] rounded-[0.3rem] border-none [color-scheme:dark] p-[0.3rem]"
-            placeholder="&#x1F5D3;"
-            onKeyDown={(e) => {
-              e.preventDefault();
-            }}
-            // ref={datePickerRef}
-            type="date"
-            dir="auto"
-            name="start-date-picker"
-            min="1950-01-01"
-            max={new Date().toISOString().split("T")[0]}
-          ></input>
-        </div>
-      </section>
-      <section className="mx-auto mb-5">
-        <IonCheckbox labelPlacement="stacked">Fajr</IonCheckbox>
-        <IonCheckbox labelPlacement="stacked">Dhuhr</IonCheckbox>
-        <IonCheckbox labelPlacement="stacked">Asr</IonCheckbox>
-        <IonCheckbox labelPlacement="stacked">Maghrib</IonCheckbox>
-        <IonCheckbox labelPlacement="stacked">Isha</IonCheckbox>
-      </section>
-      {/*  ${selectedStartDate ? "opacity-100" : "opacity-20"} */}
-      <button
-        className={`text-base border-none rounded-xl bg-[#5c6bc0] text-white w-[90%] p-3 mx-auto mb-5
+        {/*  ${selectedStartDate ? "opacity-100" : "opacity-20"} */}
+        <button
+          className={`text-base border-none rounded-xl bg-[#5c6bc0] text-white w-[90%] p-3 mx-auto mb-5
          
           `}
-        // onClick={async () => {
-        //   if (selectedStartDate) {
-        //     const todaysDate = startOfDay(new Date());
-        //     const selectedDate = startOfDay(new Date(selectedStartDate));
+          // onClick={async () => {
+          //   if (selectedStartDate) {
+          //     const todaysDate = startOfDay(new Date());
+          //     const selectedDate = startOfDay(new Date(selectedStartDate));
 
-        //     if (isAfter(selectedDate, todaysDate)) {
-        //       showAlert(
-        //         "Invalid Date",
-        //         "Please select a date that is not in the future",
-        //       );
-        //       return;
-        //     }
-        //     modal.current?.dismiss();
-        //     setSelectedStartDate(null);
-        //     await handleStartDateChange();
-        //   }
-        // }}
-      >
-        Next
-      </button>
+          //     if (isAfter(selectedDate, todaysDate)) {
+          //       showAlert(
+          //         "Invalid Date",
+          //         "Please select a date that is not in the future",
+          //       );
+          //       return;
+          //     }
+          //     modal.current?.dismiss();
+          //     setSelectedStartDate(null);
+          //     await handleStartDateChange();
+          //   }
+          // }}
+        >
+          Next
+        </button>
+      </IonContent>
     </IonModal>
   );
 };
