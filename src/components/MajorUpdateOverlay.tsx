77b5eaf54036584f88d4b5d035383eb8eb9e2c05
mpmua <mgr_55@hotmail.com>
2026-08-20 21:47:40 +0100

    chore: update changelog

--- src/components/MajorUpdateOverlay.tsx
+++ src/components/MajorUpdateOverlay.tsx
@@ -1,6 +1,6 @@
-import { GoCalendar, GoHome } from "react-icons/go";
+import { GoCalendar } from "react-icons/go";
 import { LATEST_APP_VERSION } from "../utils/changelog";
-
+import { IoBugOutline } from "react-icons/io5";
 interface MajorUpdateOverlayProps {
   setShowMajorUpdateOverlay: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -16,7 +16,7 @@ const MajorUpdateOverlay = ({
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: "rgb(36, 36, 36)",
+        backgroundColor: "rgb(20, 20, 20)",
         color: "#fff",
         padding: 20,
         zIndex: 9999,
@@ -31,45 +31,47 @@ const MajorUpdateOverlay = ({
       <section className="flex flex-col justify-center h-full">
         {/* <p className="mb-2 opacity-70">Version 4.8</p> */}
 
-        <p className="bg-[#9332ed] py-2 px-2 rounded-2xl w-fit text-lg">
-          {/* MAJOR UPDATE  */}
-          Version {LATEST_APP_VERSION}
+        <p className="bg-[#9332ed] py-2 px-2 rounded-lg w-fit text-sm font-bold">
+          MAJOR UPDATE
+          {/* Version {LATEST_APP_VERSION} */}
         </p>
 
-        <p className="mt-5 text-2xl">
-          Date navigation is now faster and easier
+        <p className="mt-2 text-2xl font-extrabold">
+          New Feature: Update Multiple Salahs
         </p>
 
-        <section className="mb-[10rem]">
-          <div className="flex items-center mt-5 mb-2">
+        <section className="mb-[10rem] mt-6">
+          <div className="flex mt-5 mb-2">
             <div>
-              <GoCalendar className="mr-5 text-3xl text-[#c583f1]" />
+              <GoCalendar className="mr-5 mt-1 text-xl text-[#c583f1]" />
             </div>
 
             <div>
-              <h2 style={{ color: "#c583f1" }}>Calendar Navigation</h2>
+              <h2 className="m-0 text-lg" style={{ color: "#c583f1" }}>
+                Bulk Salah Updates
+              </h2>
 
-              <p className="text-sm">
-                Year navigation buttons have been added to the monthly calendar,
-                allowing you to quickly move forwards or backwards by a full
-                year.
+              <p className="mt-2 text-sm">
+                You can now update multiple Salah entries at once from the
+                Settings page across a selected date range. Select the prayers,
+                status, reasons, and notes you want to apply, then update your
+                Salah history in one go.
               </p>
             </div>
           </div>
 
-          <div className="flex items-center mb-2">
+          <div className="flex mb-2 mt-7">
             <div>
-              <GoHome className="mr-5 text-3xl text-[#f7cb22]" />
+              <IoBugOutline className="mr-5 mt-1 text-xl text-[#b4ae12]" />
             </div>
 
             <div>
-              <h2 style={{ color: "#f7cb22" }}>Homepage Navigation</h2>
+              <h2 className="m-0 text-lg text-[#b4ae12]">
+                Bug Fixes & Performance
+              </h2>
 
-              <p className="text-sm">
-                Quick navigation buttons have been added to the homepage,
-                allowing you to move forwards or backwards through your Salah
-                history by 30 days or one year. The buttons automatically appear
-                when you scroll.
+              <p className="mt-2 text-sm">
+                Fixed bugs and improved performance for a smoother experience.
               </p>
             </div>
           </div>
@@ -80,7 +82,7 @@ const MajorUpdateOverlay = ({
           onClick={() => {
             setShowMajorUpdateOverlay(false);
           }}
-          className="text-center bg-[#9332ed] p-5 rounded-3xl fixed left-1/2 -translate-x-1/2 w-[90%] bottom-5 mb-[env(safe-area-inset-bottom)]"
+          className="text-center bg-[#9332ed] p-5 rounded-xl fixed left-1/2 -translate-x-1/2 w-[90%] bottom-5 mb-[env(safe-area-inset-bottom)]"
         >
           Continue
         </button>
